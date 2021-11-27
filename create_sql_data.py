c0e0c6c9482ebbf8fc500ffa8abd72308cef311d
HEHABUSTb <msanvold@gmail.com>
2021-11-27 11:38:17 +0300

    create all tables

--- create_sql_data.py
+++ create_sql_data.py
@@ -1,27 +1,71 @@
 import random
 import sqlite3
+import allure
+
+data_base = sqlite3.connect('game_params.db')
+sql = data_base.cursor()
 
 
 def random_value(start=0, end=20):
     return random.randint(start, end)
 
 
-def create_weapons_table():
-    data_base = sqlite3.connect('game_params.db')
-    sql = data_base.cursor()
+@allure.step
+def create_ships_table():
+    sql.execute("""CREATE TABLE IF NOT EXISTS ships (
+                'ship' TEXT,
+                'weapon' TEXT,
+                'hull' TEXT,
+                'engine' TEXT
+            )""")
+    data_base.commit()
 
+
+@allure.step
+def create_weapons_table():
     sql.execute("""CREATE TABLE IF NOT EXISTS weapons (
+            'weapon' TEXT,
             'reload speed' INT,
             'rotational speed' INT,
             'diameter' INT,
-            'power volley' INT,
+            'power_volley' INT,
             'count' INT
         )""")
     data_base.commit()
 
-    for i in range(0, 20):
-        sql.execute(f"INSERT INTO weapons VALUES (?, ?, ?, ?, ?)", (random_value(), random_value(), random_value(), random_value(), random_value()))
+    for i in range(1, 21):
+        weapon_name = f'Weapon-{i}'
+        sql.execute(f"INSERT INTO weapons VALUES (?, ?, ?, ?, ?, ?)", (weapon_name, random_value(),
+                                                                       random_value(), random_value(), random_value(), random_value()))
         data_base.commit()
 
 
-create_weapons_table()
+@allure.step
+def create_hulls_table():
+    sql.execute("""CREATE TABLE IF NOT EXISTS hulls (
+                'hull' TEXT,
+                'armor' INT,
+                'type' INT,
+                'capacity' INT
+            )""")
+    data_base.commit()
+
+    for i in range(1, 6):
+        hull_name = f'Hull-{i}'
+        sql.execute(f"INSERT INTO hulls VALUES ('{hull_name}', {random_value()}, {random_value()}, {random_value()})")
+        data_base.commit()
+
+
+@allure.step
+def create_engines_table():
+    sql.execute("""CREATE TABLE IF NOT EXISTS engines (
+                'engine' TEXT,
+                'power' INT,
+                'type' INT
+            )""")
+    data_base.commit()
+
+    for i in range(1, 7):
+        engine_name = f'Engine-{i}'
+        sql.execute(f"INSERT INTO engines VALUES ('{engine_name}', {random_value()}, {random_value()})")
+        data_base.commit()
