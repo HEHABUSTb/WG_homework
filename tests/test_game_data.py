3c398df83a553a0bb7a5f9eca6fb3fdb6876f1ce
a_lyubinskiy <msanvold@gmail.com>
2021-11-30 10:53:03 +0300

    add comments, fix set(str) in assert

--- tests/test_game_data.py
+++ tests/test_game_data.py
@@ -14,6 +14,7 @@ class TestGameParams(BaseClass):
 
     @pytest.fixture(params=ships)
     def get_data(self, request):
+        """Will need to rework in smth like param = pytest.param(ship.name, hull_name, ship, hull, hulls_count, id='{}-{}'.format(ship.name, hull_name))"""
         return request.param
 
     @pytest.mark.one
@@ -22,8 +23,11 @@ class TestGameParams(BaseClass):
         print(get_data)
         name = get_data['ship']
         print('FAKE DATA')
-        print(self.get_fake_data(name))
-        pass
+        fake = self.get_fake_data(name)
+        print(fake['engine'] == get_data['engine'])
+        engine_name, engine_power = get_data['engine'], float(get_data['power'])
+        fake_engine_name, fake_engine_power = fake['engine'], float(fake['power'])
+        assert (engine_name, engine_power) == (fake_engine_name, fake_engine_power)
 
     @pytest.mark.engines
     def test_engines(self, get_data):
@@ -32,7 +36,7 @@ class TestGameParams(BaseClass):
         fake_data = self.get_fake_data(get_data['ship'])
         fake_engine_name, fake_engine_power, fake_engine_type = fake_data['engine'], float(fake_data['power']), float(fake_data['type'])
 
-        assert (set(engine_name), engine_power, engine_type) == (set(fake_engine_name), fake_engine_power, fake_engine_type),\
+        assert (engine_name, engine_power, engine_type) == (fake_engine_name, fake_engine_power, fake_engine_type),\
             f"Values not equal got {engine_name}, power:{engine_power}, type:{engine_type}" \
             f" should be {fake_engine_name}, {fake_engine_power}, {fake_engine_type}"
 
@@ -45,8 +49,8 @@ class TestGameParams(BaseClass):
         fake_hull_name, fake_hull_armor, fake_hull_type, fake_hull_capacity =\
             fake_data['hull'], int(fake_data['armor']), int(fake_data['types']), int(fake_data['capacity'])
 
-        assert (set(hull_name), hull_armor, hull_type, hull_capacity) ==\
-               (set(fake_hull_name), fake_hull_armor, fake_hull_type, fake_hull_capacity), \
+        assert (hull_name, hull_armor, hull_type, hull_capacity) ==\
+               (fake_hull_name, fake_hull_armor, fake_hull_type, fake_hull_capacity), \
                f"Values not equal got {hull_name} armor: {hull_armor}, type: {hull_type}, capasity: {hull_capacity}" \
                f" should be {fake_hull_name}, {fake_hull_armor}, {fake_hull_type} {fake_hull_capacity}"
 
@@ -60,7 +64,7 @@ class TestGameParams(BaseClass):
                                                                                       int(fake_data['rotational speed'])
         fake_diameter, fake_power, fake_count = int(fake_data['diameter']), int(fake_data['power_volley']),\
                                                                                                 int(fake_data['count'])
-        assert (set(name), reload_speed, rotation, diameter, power, count) == \
+        assert (name, reload_speed, rotation, diameter, power, count) == \
                (fake_name, fake_reload_speed, fake_rotation, fake_diameter, fake_diameter, fake_power, fake_count), \
                 f"Values not equal got {name} reload speed: {reload_speed}, rotation: {rotation}, diameter: {diameter}" \
                 f" count:{count} should be {fake_name, fake_reload_speed, fake_rotation, fake_diameter, fake_power, fake_count}"
