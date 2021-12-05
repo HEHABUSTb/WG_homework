9a16f890a7c6c58830912f200dd80f0fbb4ad142
HEHABUSTb <msanvold@gmail.com>
2021-12-05 17:17:51 +0300

    few visual improvements

--- TestData/GUI.py
+++ TestData/GUI.py
@@ -6,29 +6,29 @@ def create_table():
     try:
         create = CreateDB()
         value = int(entry_text.get())
+
         create.create_ships_table(how_many_ships=value)
         create.create_engines_table()
         create.create_hulls_table()
         create.create_weapons_table()
+
         messagebox.showinfo(message='Success!', title='Success message')
     except Exception as e:
         messagebox.showerror(title='Error', message=f"Something got wrong {e}")
 
 
 window = Tk()
-window.geometry('240x120')
+window.geometry('240x100')
 window.title('Create table')
 
-
-label = Label(window, text='How many ships do you want?', font=('Arial', 10, 'bold'))
-
+label = Label(window, text='How many ships do you want?', font=('Arial', 10, 'bold'), pady=5)
 label.pack()
+
 entry_text = Entry(window, text=200)
 entry_text.insert(0, '200')
-entry_text.pack()
+entry_text.pack(pady=5)
 
 button = Button(window, text='Create', command=create_table)
-button.pack()
-
+button.pack(pady=5)
 
 window.mainloop()
